1a2a4d0e3a7521534b6840eef57b8b55a55893d8
AndrewVasin <ironfeyd@gmail.com>
2023-11-27 22:53:46 +0300

    project update

--- src/main/java/com/vasin/diabetic_diary_rest/model/SugarLevelRecord.java
+++ src/main/java/com/vasin/diabetic_diary_rest/model/SugarLevelRecord.java
@@ -2,14 +2,16 @@ package com.vasin.diabetic_diary_rest.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "sugar_level")
-public class SugarLevelRecord {
+public class SugarLevelRecord implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "date")
--- src/main/java/com/vasin/diabetic_diary_rest/model/User.java
+++ src/main/java/com/vasin/diabetic_diary_rest/model/User.java
@@ -2,13 +2,15 @@ package com.vasin.diabetic_diary_rest.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
 @Table(name = "users_table")
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "first_name")
